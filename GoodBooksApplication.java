7ebcc45978f792b862d73a1877a3693854edb11a
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2022-11-07 19:49:28 -0500

    Add files via upload

---
+++ GoodBooksApplication.java
@@ -0,0 +1,13 @@
+package com.alkyz04.goodbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodBooksApplication.class, args);
+    }
+
+}
---
+++ Rating.java
@@ -0,0 +1,8 @@
+package com.alkyz04.goodbooks;
+
+public class Rating {
+    public String username;
+    public String dateStamp;
+    public int stars;
+    public String comment;
+}
