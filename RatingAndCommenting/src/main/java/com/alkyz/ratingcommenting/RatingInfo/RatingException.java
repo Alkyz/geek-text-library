de1948e8bcaf83c6f580970f1d67c9f18bcdc068
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2022-12-03 21:36:24 -0500

    Revert "Rating And Commenting Comitt"
    This reverts commit 1011fba6cc676a5d0552f21f33ad8dfeadffeecc.

--- RatingAndCommenting/src/main/java/com/alkyz/ratingcommenting/RatingInfo/RatingException.java
+++
@@ -1,7 +0,0 @@
-package com.alkyz.ratingcommenting.RatingInfo;
-
-public class RatingException extends Exception {
-    public RatingException (String errorMessage) {
-        super(errorMessage);
-    }
-}
