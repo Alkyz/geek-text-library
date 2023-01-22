2306da9ac6fca491f115c9d8b1b474fefb1fb6a9
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2023-01-22 18:03:53 -0500

    Create profile_of_user_credit_card.java

---
+++ ProfileOfUser/profile_of_user_credit_card.java
@@ -0,0 +1,108 @@
+package software1.softwareengineering1.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Entity
+public class profile_of_user_credit_card {
+    @Id
+    @GeneratedValue
+    private int PK;
+    private String CardType;
+    private String CreditCardNumber;
+    private String NameOfCreditCard;
+    private int CsvCode;
+    private String CreditCardExpirationDateMonth;
+    private String CreditCardExpirationDateYear;
+    private String UserEmailFK;
+
+    public String getCardType() {
+        return CardType;
+    }
+
+    public void setCardType(String cardType) {
+        CardType = cardType;
+    }
+
+    public String getCreditCardNumber() {
+        return CreditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        CreditCardNumber = creditCardNumber;
+    }
+
+    public String getNameOfCreditCard() {
+        return NameOfCreditCard;
+    }
+
+    public void setNameOfCreditCard(String nameOfCreditCard) {
+        NameOfCreditCard = nameOfCreditCard;
+    }
+
+    public int getCsvCode() {
+        return CsvCode;
+    }
+
+    public void setCsvCode(int csvCode) {
+        CsvCode = csvCode;
+    }
+
+    public String getCreditCardExpirationDateMonth() {
+        return CreditCardExpirationDateMonth;
+    }
+
+    public void setCreditCardExpirationDateMonth(String creditCardExpirationDateMonth) {
+        CreditCardExpirationDateMonth = creditCardExpirationDateMonth;
+    }
+
+    public String getCreditCardExpirationDateYear() {
+        return CreditCardExpirationDateYear;
+    }
+
+    public void setCreditCardExpirationDateYear(String creditCardExpirationDateYear) {
+        CreditCardExpirationDateYear = creditCardExpirationDateYear;
+    }
+
+    public String getUserEmailFK() {
+        return UserEmailFK;
+    }
+
+    public void setUserEmailFK(String userEmailFK) {
+        UserEmailFK = userEmailFK;
+    }
+
+    public int getPK() {
+        return PK;
+    }
+
+    public void setPK(int PK) {
+        this.PK = PK;
+    }
+
+
+
+    public profile_of_user_credit_card(String cardType, String creditCardNumber,
+                                       String nameOfCreditCard, int csvCode, String creditCardExpirationDateMonth,
+                                       String creditCardExpirationDateYear, String userEmailFK, int PK) {
+        CardType = cardType;
+        CreditCardNumber = creditCardNumber;
+        NameOfCreditCard = nameOfCreditCard;
+        CsvCode = csvCode;
+        CreditCardExpirationDateMonth = creditCardExpirationDateMonth;
+        CreditCardExpirationDateYear = creditCardExpirationDateYear;
+        UserEmailFK = userEmailFK;
+        this.PK = PK;
+    }
+
+    public profile_of_user_credit_card(){
+
+    }
+
+//end
+}
